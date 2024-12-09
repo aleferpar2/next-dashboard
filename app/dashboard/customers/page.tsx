da929975b0508c170487e7078a0164f71e36470f
aleferpar <aleferpar2@alu.edu.gva.es>
2024-12-09 15:45:39 +0000

    a

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Dashboard() {
+return <h1>Hola customer</h1>;
+    
+
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function Dashboard() {
+return <h1>Hola Bienvenido a la grieta del fornicador</h1>;
+    
+
+}
